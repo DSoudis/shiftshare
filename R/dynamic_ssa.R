39b33f7ddce6cf9705ff595d96ab4e5b55f291e3
DSoudis <soudis.di@gmail.com>
2017-05-12 21:06:10 +0200

    used double colon approach to call external packages

--- R/dynamic_ssa.R
+++ R/dynamic_ssa.R
@@ -1,14 +1,12 @@
 
 # Calculates the yearly national change in Y within two points in time
 get.Gk <- function(df, y.var ,region.var, year.var, sector.var, gregion.id, sector.id, start.year, end.year){
-  require(dplyr)
-  require(lazyeval)
-  filter.criteria <- interp(~(between(y1, x1, x2)) & (y3 == x3) &
+  filter.criteria <- lazyeval::interp(~(dplyr::between(y1, x1, x2)) & (y3 == x3) &
                               (y4 == x4), .values = list( y1 = as.name(year.var), x1 = start.year, x2 = end.year,
                                                           y3 = as.name(region.var), x3 = gregion.id,
                                                           y4 = as.name(sector.var), x4 = sector.id))
   
-  temp.Gk <- df %>% filter_(filter.criteria) %>% select_(year.var, y.var)
+  temp.Gk <- df %>% dplyr::filter_(filter.criteria) %>% dplyr::select_(year.var, y.var)
   
   Gk <- (temp.Gk[[y.var]] - lag(temp.Gk[[y.var]])) / lag(temp.Gk[[y.var]])
   Gk <- data.frame(cbind(start.year:end.year, Gk))
@@ -19,58 +17,54 @@ get.Gk <- function(df, y.var ,region.var, year.var, sector.var, gregion.id, sect
 
 # Calculates the national yearly change in Y for each sector within two points in time
 get.Gik <- function(df, y.var ,region.var, year.var, sector.var, gregion.id, sector.id, start.year, end.year){
-  require(dplyr)
-  require(lazyeval)
-  filter.criteria <- interp(~(between(y1, x1, x2)) & (y3 == x3) &
+  filter.criteria <- lazyeval::interp(~(dplyr::between(y1, x1, x2)) & (y3 == x3) &
                               (y4 != x4), .values = list( y1 = as.name(year.var), x1 = start.year, x2 = end.year,
                                                           y3 = as.name(region.var), x3 = gregion.id,
                                                           y4 = as.name(sector.var), x4 = sector.id))
   
-  temp.Gik <- df %>% filter_(filter.criteria) %>% select_(region.var, year.var ,sector.var, y.var)
+  temp.Gik <- df %>% dplyr::filter_(filter.criteria) %>% dplyr::select_(region.var, year.var ,sector.var, y.var)
   
-  mutate.call <- interp(~((a - lag(a))/lag(a)), a = as.name(y.var))
-  Gik <- temp.Gik %>% group_by_(sector.var) %>% mutate_(.dots = setNames(list(mutate.call), 'changeGik')) %>% select_(sector.var, year.var ,'changeGik')
+  mutate.call <- lazyeval::interp(~((a - lag(a))/lag(a)), a = as.name(y.var))
+  Gik <- temp.Gik %>% dplyr::group_by_(sector.var) %>% dplyr::mutate_(.dots = setNames(list(mutate.call), 'changeGik')) %>% dplyr::select_(sector.var, year.var ,'changeGik')
   Gik
 }
 
 
 # Calculates yearly change in Y by region and sector within two points in time
 get.gik <- function(df, y.var, region.var, year.var, sector.var, gregion.id, sector.id, start.year, end.year){
-  require(dplyr)
-  require(lazyeval)
-  filter.criteria <- interp(~(between(y1, x1, x2)) & (y3 != x3) &
+  filter.criteria <- lazyeval::interp(~(dplyr::between(y1, x1, x2)) & (y3 != x3) &
                               (y4 != x4), .values = list( y1 = as.name(year.var), x1 = start.year, x2 = end.year,
                                                           y3 = as.name(region.var), x3 = gregion.id,
                                                           y4 = as.name(sector.var), x4 = sector.id))
   
-  temp.gik <- df %>% filter_(filter.criteria) %>% select_(region.var, year.var ,sector.var, y.var)
+  temp.gik <- df %>% dplyr::filter_(filter.criteria) %>% dplyr::select_(region.var, year.var ,sector.var, y.var)
   temp.gik
   
-  mutate.call <- interp(~((a - lag(a))/lag(a)), a = as.name(y.var))
-  gik <- temp.gik %>% group_by_(region.var, sector.var) %>% mutate_(.dots = setNames(list(mutate.call), 'changegik')) %>%
-    select_(sector.var, year.var ,'changegik') %>% arrange_(region.var, sector.var, year.var)
+  mutate.call <- lazyeval::interp(~((a - lag(a))/lag(a)), a = as.name(y.var))
+  gik <- temp.gik %>% dplyr::group_by_(region.var, sector.var) %>% dplyr::mutate_(.dots = setNames(list(mutate.call), 'changegik')) %>%
+    dplyr::select_(sector.var, year.var ,'changegik') %>% dplyr::arrange_(region.var, sector.var, year.var)
   gik
 }
 
 
 # Extracts yearly employment by sector and region
 get.etk <- function(df, y.var, region.var, year.var, sector.var, gregion.id, sector.id, start.year, end.year){
-  filter.criteria <- interp(~(between(y1, x1, x2)) & y3 != x3 & y4 != x4, .values = list(y1 = as.name(year.var), x1 = start.year, x2 = end.year,
+  filter.criteria <- lazyeval::interp(~(dplyr::between(y1, x1, x2)) & y3 != x3 & y4 != x4, .values = list(y1 = as.name(year.var), x1 = start.year, x2 = end.year,
                                                                                          y3 = as.name(sector.var), x3 = sector.id,
                                                                                          y4 = as.name(region.var), x4 = gregion.id))
-  mutate.call <- interp(~lag(a, 1), a = as.name(y.var))
+  mutate.call <- lazyeval::interp(~lag(a, 1), a = as.name(y.var))
   
-  etk <- df %>% filter_(filter.criteria) %>% select_(region.var, sector.var, year.var ,y.var) %>% arrange_(region.var, sector.var, year.var)
-  etk <- etk %>% group_by_(region.var, sector.var) %>%  mutate_(.dots = setNames(list(mutate.call), 'laged'))
+  etk <- df %>% dplyr::filter_(filter.criteria) %>% dplyr::select_(region.var, sector.var, year.var ,y.var) %>% dplyr::arrange_(region.var, sector.var, year.var)
+  etk <- etk %>% dplyr::group_by_(region.var, sector.var) %>%  dplyr::mutate_(.dots = setNames(list(mutate.call), 'laged'))
   etk
 }
 
 
 #Merges previous results
 get.merged <- function(etk, Gik, gik, Gk, region.var, sector.var, year.var){
-  merged <- left_join(etk, Gik, by = c(sector.var, year.var)) %>%
-    left_join(.,gik, by = c(region.var, sector.var, year.var)) %>%
-    left_join(.,Gk)
+  merged <- dplyr::left_join(etk, Gik, by = c(sector.var, year.var)) %>%
+    dplyr::left_join(.,gik, by = c(region.var, sector.var, year.var)) %>%
+    dplyr::left_join(.,Gk)
   merged
 }
 
@@ -84,7 +78,7 @@ get.dssa <- function(merged, y.var, region.var, sector.var, year.var){
   merged$IMik <- IMik[,1]
   merged$RSik <- RSik[,1]
   
-  results <- merged %>% group_by_(region.var, sector.var) %>% summarise(sum(NSik, na.rm = T),
+  results <- merged %>% dplyr::group_by_(region.var, sector.var) %>% dplyr::summarise(sum(NSik, na.rm = T),
                                                                         sum(IMik, na.rm = T),
                                                                         sum(RSik, na.rm = T))
   names(results) <- c(region.var, sector.var, 'NSik', 'IMik', 'RSik')
