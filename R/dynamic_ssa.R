82fc577adaaa97e4770fc445b341163f641a845f
DSoudis <soudis.di@gmail.com>
2018-08-21 16:35:53 +0200

    used the double colon method to call dplyr

--- R/dynamic_ssa.R
+++ R/dynamic_ssa.R
@@ -24,7 +24,7 @@ get.Gik <- function(df, y.var ,region.var, year.var, sector.var, gregion.id, sec
   
   temp.Gik <- df %>% dplyr::filter_(filter.criteria) %>% dplyr::select_(region.var, year.var ,sector.var, y.var)
   
-  mutate.call <- lazyeval::interp(~((a - lag(a))/lag(a)), a = as.name(y.var))
+  mutate.call <- lazyeval::interp(~((a - dplyr::lag(a))/dplyr::lag(a)), a = as.name(y.var))
   Gik <- temp.Gik %>% dplyr::group_by_(sector.var) %>% dplyr::mutate_(.dots = setNames(list(mutate.call), 'changeGik')) %>% dplyr::select_(sector.var, year.var ,'changeGik')
   Gik
 }
@@ -40,7 +40,7 @@ get.gik <- function(df, y.var, region.var, year.var, sector.var, gregion.id, sec
   temp.gik <- df %>% dplyr::filter_(filter.criteria) %>% dplyr::select_(region.var, year.var ,sector.var, y.var)
   temp.gik
   
-  mutate.call <- lazyeval::interp(~((a - lag(a))/lag(a)), a = as.name(y.var))
+  mutate.call <- lazyeval::interp(~((a - dplyr::lag(a))/dplyr::lag(a)), a = as.name(y.var))
   gik <- temp.gik %>% dplyr::group_by_(region.var, sector.var) %>% dplyr::mutate_(.dots = setNames(list(mutate.call), 'changegik')) %>%
     dplyr::select_(sector.var, year.var ,'changegik') %>% dplyr::arrange_(region.var, sector.var, year.var)
   gik
@@ -52,7 +52,7 @@ get.etk <- function(df, y.var, region.var, year.var, sector.var, gregion.id, sec
   filter.criteria <- lazyeval::interp(~(dplyr::between(y1, x1, x2)) & y3 != x3 & y4 != x4, .values = list(y1 = as.name(year.var), x1 = start.year, x2 = end.year,
                                                                                          y3 = as.name(sector.var), x3 = sector.id,
                                                                                          y4 = as.name(region.var), x4 = gregion.id))
-  mutate.call <- lazyeval::interp(~lag(a, 1), a = as.name(y.var))
+  mutate.call <- lazyeval::interp(~dplyr::lag(a, 1), a = as.name(y.var))
   
   etk <- df %>% dplyr::filter_(filter.criteria) %>% dplyr::select_(region.var, sector.var, year.var ,y.var) %>% dplyr::arrange_(region.var, sector.var, year.var)
   etk <- etk %>% dplyr::group_by_(region.var, sector.var) %>%  dplyr::mutate_(.dots = setNames(list(mutate.call), 'laged'))
